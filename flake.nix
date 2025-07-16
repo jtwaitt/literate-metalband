b544e394b0c675838c3366ab7e69aa86ea7c60e7
agentelement <agentelement@agentelement.net>
2025-07-15 17:49:08 -0700

    build: nix and uv

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3;
+        pythonEnv = python.withPackages (pkgs: [
+          pkgs.numpy
+          # Here goes all the libraries that can't be managed by uv because of dynamic linking issues
+          # or that you just want to be managed by nix for one reason or another
+        ]);
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              uv
+              python
+              pythonEnv
+            ];
+
+            shellHook = ''
+              export UV_PYTHON_PREFERENCE="only-system";
+              export UV_PYTHON=${python}
+            '';
+          };
+      }
+    );
+}
